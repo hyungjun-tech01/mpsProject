1d5559958f6a70ca75db8c15483a9fc0b8870a22
C.H.S <newtons2002@gmail.com>
2025-02-12 20:47:32 +0900

    (edit-items)원화 icon 변경

--- app/components/edit-items.tsx
+++ app/components/edit-items.tsx
@@ -100,7 +100,7 @@ export function EditItem({
               />
               {(!locale || locale === "ko") &&
                   <div className="pointer-events-none absolute left-3 top-[16px] h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900">
-                    <CurrencyWonIcon  />
+                    <CurrencyWonIcon  className="pointer-events-none absolute left-1 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
                   </div>
               }
               {!!locale && locale === "en" && (
