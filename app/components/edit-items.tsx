2df02848663f6878d88445a34cfc7e981a89018c
hyungsung <hyungsung@sindoh.com>
2025-02-12 17:42:09 +0900

    (edit-items) 한국 원화에 대한 icon 추가

--- app/components/edit-items.tsx
+++ app/components/edit-items.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { ChangeEvent, useState } from "react";
 import PaidOutlinedIcon from "@mui/icons-material/PaidOutlined";
+import CurrencyWonIcon from "@/app/components/icons/CurrencyWonIcon"
 import LineChart from "@/app/components/lineChart";
 
 
@@ -98,13 +98,12 @@ export function EditItem({
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
                 required
               />
-              {!locale ||
-                (locale === "ko" && (
-                  <div className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900">
-                    {' '}
+              {(!locale || locale === "ko") &&
+                  <div className="pointer-events-none absolute left-3 top-[16px] h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900">
+                    <CurrencyWonIcon  />
                   </div>
-                ))}
-              {locale && locale === "en" && (
+              }
+              {!!locale && locale === "en" && (
                 <PaidOutlinedIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
               )}
             </div>
---
+++ app/components/icons/CurrencyWonIcon.jsx
@@ -0,0 +1,12 @@
+"use strict";
+"use client";
+
+const CurrencyWonIcon = function () {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="#555555">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2m0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8 M6.08 8.5 l2 9 h2.52 l1.41 -6.36 l1.41 6.36 h2.52 l2 -9 h-1.84 l-1.41 6.36 l-1.41 -6.36 h-2.52 l-1.41 6.36 l-1.41 -6.36 M5 12.5 h14 v-1.8 h-14 M5 15 h14 v-1.8 h-14"/>
+        </svg>
+    )
+}
+
+export default CurrencyWonIcon;
