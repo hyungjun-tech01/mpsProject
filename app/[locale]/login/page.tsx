7766de8b545545fb00180753314f912d80d225cf
hyungsung <newtons2002@gmail.com>
2025-02-16 22:55:22 +0900

    [auth] 작업 중

--- app/[locale]/login/page.tsx
+++ app/[locale]/login/page.tsx
@@ -1,13 +1,14 @@
-import LoginForm from '@/app/components/auth/login-form';
 import { Suspense } from 'react';
+import clsx from 'clsx';
+import LoginForm from '@/app/components/auth/login-form';
  
 export default function LoginPage() {
   return (
     <main className="flex items-center justify-center md:h-screen">
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-          <div className="w-32 text-white md:w-36">
-            {' '}
+        <div className="flex h-20 w-full items-end rounded-lg bg-lime-900 p-3 md:h-36" >
+          <div className="w-32 text-white text-2xl font-medium md:w-36">
+            {'MPS Next'}
           </div>
         </div>
         <Suspense>
